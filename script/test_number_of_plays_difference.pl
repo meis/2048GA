310f6764546c7449907457cbacc5502acf272344
Marc Egea i Sala <marc.egea.sala@gmail.com>
2017-04-29 15:05:39 +0000

    Fix script to check imporance of number of plays

--- script/test_number_of_plays_difference.pl
+++ script/test_number_of_plays_difference.pl
@@ -2,9 +2,10 @@
 use v5.10;
 use FindBin;
 use lib "$FindBin::Bin/../lib";
-use Minion::Chromosome;
+use Chromosome::40Bits;
+use Player;
 
-my $tests = 10;
+my $tests = 100;
 my @times = (2000, 1000, 500, 100, 50, 20, 10);
 my @deviation  = map { 0 } @times;
 
@@ -12,19 +13,16 @@ for my $test (1..$tests) {
     my $bits = [ map { int(rand(2)) % 2} 0..39 ];
     my @fitnesses;
 
-    say "Test $test";
-    say "Chromosome: " .  join('', @$bits);
-
     my $n = 0;
     for my $how_much (@times) {
-        my $fitness = Minion::Chromosome->new({ bits => $bits })->build_individual->play($how_much);
+        my $chromosome = Chromosome::40Bits->new({ genes => $bits });
+        my $player = Player->new({ chromosome => $chromosome });
+        my $fitness = $player->play($how_much);
+
         push @fitnesses, $fitness;
         my $diff = (($fitness - $fitnesses[0]) / (($fitness + $fitnesses[0]) / 2)) * 100;
         $deviation[$n++] += abs $diff;
-        say " $how_much times: $fitness wich is $diff";
     }
-
-    say ".....................";
 }
 
 
